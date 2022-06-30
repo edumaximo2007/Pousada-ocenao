1d748b4bcb7edeaacff75bb7031772b3938702a0
edumaximo2007 <edumaximo2007@hotmail.com>
2022-06-30 18:18:35 -0300

    simplificando o codigo

--- script/main.js
+++ script/main.js
@@ -1,17 +1,6 @@
-const btnMobile = document.getElementById('btn-mobile');
+const navToggle = document.querySelector(".btn-mobile")
+const links = document.querySelector(".menu")
 
-function toggleMenu(event) {
-  if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.querySelector('nav.nav');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-  }
-}
-
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+navToggle.addEventListener("click", function(){
+  links.classList.toggle("ativo")
+});
